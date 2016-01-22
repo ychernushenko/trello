1c8f25757ad004bdda51bb5d4a7c3aee2a3982da
Yury Chernushenko <y.chernushenko@gmail.com>
2016-01-22 04:52:01 -0500

    Created templates

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^trello/', include('trello.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- django_project/trello/views.py
+++ django_project/trello/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from trello.models import Status
+from trello.models import Task
 
-# Create your views here.
+def index(request):
+    status_list = Status.objects.all()
+    task_list = Task.objects.all()
+    context = {'status_list': status_list,
+               'task_list': task_list
+    }
+    return render(request, 'trello/index.html', context)

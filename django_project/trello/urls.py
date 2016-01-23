9939658df98e2616c353b4968459b81195ed8e68
Yury Chernushenko <y.chernushenko@gmail.com>
2016-01-23 16:21:22 -0500

    Saving updates to database

--- django_project/trello/urls.py
+++ django_project/trello/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from trello import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^update_pos$', views.ajax, name='ajax'),
 )
--- django_project/trello/views.py
+++ django_project/trello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from trello.models import Status
 from trello.models import Task
+import json
 
 def index(request):
     status_list = Status.objects.all()
@@ -9,3 +11,10 @@ def index(request):
                'task_list': task_list
     }
     return render(request, 'trello/index.html', context)
+
+def ajax(request):
+    selected_task = Task.objects.get(pk=request.POST['task_id'])
+    selected_status = Status.objects.get(pk=request.POST['status_id'])
+    selected_task.status = selected_status
+    selected_task.save()
+    return HttpResponse(json.dumps({'status': 'OK'}), content_type="application/json")

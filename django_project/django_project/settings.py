af3ac40a305944dab8fbc163225e3e76b47e32ab
Yury Chernushenko <y.chernushenko@gmail.com>
2016-02-02 07:42:01 -0500

    Last

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'swampdragon',
     'trello',
 )
 
@@ -84,3 +85,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
+
+# SwampDragon settings
+SWAMP_DRAGON_CONNECTION = ('swampdragon.connections.sockjs_connection.DjangoSubscriberConnection', '/data')
